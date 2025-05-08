4d38d5a77f342c054803fe08490ea40c8caf6d53
Adrien Russo <adrien.russo@gmail.com>
2025-05-08 18:25:15 +0200

    feat: uniformise la taille des icônes et améliore le style des boutons dans la liste des catégories

--- src/components/icons/MdiMagnify.vue
+++ src/components/icons/MdiMagnify.vue
@@ -3,7 +3,7 @@
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
     fill="currentColor"
-    class="mdi-icon"
+    width="20" height="20"
   >
     <path
       d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.5,12.58 14.58,13.75L20.39,19.56L19.56,20.39L13.75,14.58C12.58,15.5 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5A4.5,4.5 0 0,0 5,9.5A4.5,4.5 0 0,0 9.5,14A4.5,4.5 0 0,0 14,9.5A4.5,4.5 0 0,0 9.5,5Z"
@@ -16,10 +16,3 @@ export default {
   name: "MdiMagnify",
 };
 </script>
-
-<style scoped>
-.mdi-icon {
-  width: 24px;
-  height: 24px;
-}
-</style>
