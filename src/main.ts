de2325c26cf23975e3ad60413a76bdffdde284ba
Adrien Russo <adrien.russo@gmail.com>
2025-05-11 15:27:02 +0200

    feat: integrate @nuxt/ui and update project configuration
    - Added @nuxt/ui as a dependency in package.json.
    - Wrapped AppHeader and RouterView components with UApp in App.vue.
    - Imported @nuxt/ui styles in main.css.
    - Registered @nuxt/ui plugin in main.ts.
    - Created tsconfig.app.json for improved TypeScript support.
    - Updated tsconfig.json to include additional TypeScript files.
    - Configured vite.config.js to use @nuxt/ui plugin.

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import router from './router'
 import './assets/main.css'
 import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
 import 'vue-multiselect/dist/vue-multiselect.css'
+import ui from '@nuxt/ui/vue-plugin'
 
 const app = createApp(App)
 app.use(router)
+app.use(ui)
 
 // Ajout de TanStack Query
 const queryClient = new QueryClient()
